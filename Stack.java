e5d86b8490c107078563d441bee10fbd1eae7bb3
ivangoldov <47887209+ivangoldov@users.noreply.github.com>
2019-02-23 23:24:53 +0300

    added task 1 (stack)

---
+++ Stack.java
@@ -0,0 +1,86 @@
+package group144.goldov;
+
+public class Stack {
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Node head;
+    private int length;
+
+    Stack() {
+        head = null;
+        length = 0;
+    }
+
+    /**
+     * method that checks if stack is empty
+     *
+     */
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    /**
+     * method that pushes element to stack
+     *
+     */
+    public void push(int value) {
+        head = new Node(value, head);
+        length++;
+    }
+
+    /**
+     * method that pops element from stack
+     *
+     */
+    public int pop() {
+        if (isEmpty()) {
+            return Integer.MIN_VALUE;
+        }
+        length--;
+        int temporaryValue = head.value;
+        head = head.next;
+        return temporaryValue;
+    }
+
+    /**
+     * method that shows the last element in stack
+     *
+     */
+    public int peek() {
+        if (isEmpty()) {
+            return Integer.MIN_VALUE;
+        }
+        return head.value;
+    }
+
+    /**
+     * method that shows legth of the stack
+     *
+     */
+    public int getLength() {
+        return length;
+    }
+
+    /**
+     * method that prints stack
+     */
+    public void print() {
+        if (isEmpty()) {
+            return;
+        }
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.value + " ");
+            current = current.next;
+        }
+    }
+}
