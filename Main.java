3a02b94577a93ba74a1ef2d6a87f5e6ceffbe45a
ivangoldov <47887209+ivangoldov@users.noreply.github.com>
2019-02-23 23:34:53 +0300

    added task 2 (linked list)

---
+++ Main.java
@@ -0,0 +1,29 @@
+package group144.goldov;
+
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(5);
+        list.add(15);
+        list.add(75);
+        list.add(52);
+        System.out.print("Printed list : ");
+        list.print();
+        System.out.println();
+        System.out.println("List length = " + list.getLength());
+        System.out.println("15's index = " + list.find(15));
+        list.remove(15);
+        System.out.print("Printed list : ");
+        list.print();
+        System.out.println();
+        System.out.println("List length = " + list.getLength());
+        list.add(24, 1);
+        System.out.print("Printed list : ");
+        list.print();
+        System.out.println();
+        list.add(1, 0);
+        System.out.print("Printed list : ");
+        list.print();
+        System.out.println();
+    }
+}
---
+++ SinglyLinkedList.java
@@ -0,0 +1,138 @@
+package group144.goldov;
+
+public class SinglyLinkedList {
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        private Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private int length;
+    private Node head;
+
+    public SinglyLinkedList() {
+        length = 0;
+        head = null;
+    }
+
+    /**
+     * a method that checks if the list is empty
+     */
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    /**
+     * a method that shows the length of the list
+     */
+    public int getLength() {
+        return length;
+    }
+
+    /**
+     * a method that adds a new element to the end of the list
+     */
+    public void add(int value) {
+        if (isEmpty()) {
+            head = new Node(value, null);
+            length++;
+        }
+        else {
+            Node temporary = this.head;
+
+            while (temporary.next != null) {
+                temporary = temporary.next;
+            }
+
+            temporary.next = new Node(value, null);
+            length++;
+        }
+    }
+
+    /**
+     * a method that adds a new element after given index
+     */
+    public void add(int value, int index) {
+        if (isEmpty()) {
+            add(value);
+            return;
+        }
+        if (index > length || index < 0) {
+            return;
+        }
+
+        if (index == length) {
+            add(value);
+            return;
+        }
+
+        length++;
+        if (index == 0) {
+            Node prevNode = this.head;
+            this.head = new Node(value, prevNode);
+            return;
+        }
+
+        Node temporary = this.head;
+        for (int i = 0; i < index - 1; ++i) {
+            temporary = temporary.next;
+        }
+
+        temporary.next =  new Node(value, temporary.next);
+
+    }
+
+    /**
+     * a method that returns an index of given element (or -1 if the element isn't in the list)
+     */
+    public int find(int value) {
+        Node temporary = this.head;
+        int index = 0;
+        while (this.head != null) {
+            if (temporary.value == value) {
+                return index;
+            }
+
+            index++;
+            temporary = temporary.next;
+        }
+
+        return -1;
+    }
+
+    /**
+     * a method that removes the element from the list and returns false if the element isn't in the list
+     */
+    public boolean remove(int value) {
+        Node thisNode = this.head;
+        Node prevNode = null;
+        while (thisNode != null) {
+            if (thisNode.value == value) {
+                prevNode.next = thisNode.next;
+                length--;
+                return true;
+            }
+
+            prevNode = thisNode;
+            thisNode = thisNode.next;
+        }
+
+        return false;
+    }
+
+    /**
+     * a method that prints the list
+     */
+    public void print() {
+        Node temporary = this.head;
+        while (temporary != null) {
+            System.out.print(temporary.value + " ");
+            temporary = temporary.next;
+        }
+    }
+}
