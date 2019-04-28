73dddc740d5a6b6fbebe09e96bc54731896b5b32
Ivan Goldov <ivangoldov@gmail.com>
2019-04-28 20:22:48 +0300

    deleted useless class

--- sem2/homework2/task2/src/main/java/group144/goldov/NullPointerException.java
+++
@@ -1,9 +0,0 @@
-package group144.goldov;
-
-public class NullPointerException extends RuntimeException {
-    public NullPointerException() { }
-
-    public NullPointerException(String msg) {
-        super(msg);
-    }
-}
