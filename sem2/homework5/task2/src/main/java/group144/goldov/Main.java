51d5338d1fd4fa6a9cd66bc40b49b5917d68dddc
Ivan Goldov <ivangoldov@gmail.com>
2019-05-19 18:14:20 +0300

    window zoom

--- sem2/homework5/task2/src/main/java/group144/goldov/Main.java
+++ sem2/homework5/task2/src/main/java/group144/goldov/Main.java
@@ -17,7 +17,6 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
         primaryStage.setTitle("Simple calculator");
         primaryStage.show();
     }
